ffe0635132f22204de6c8a73ef733745c762d3bf
Andrew Cao <andrewcao618@gmail.com>
2025-09-20 23:33:48 -0400

    WIP Test cases

--- src/test/java/com/university/bookstore/impl/BookstoreArrayListTest.java
+++ src/test/java/com/university/bookstore/impl/BookstoreArrayListTest.java
@@ -1,6 +1,9 @@
 package com.university.bookstore.impl;
 import com.university.bookstore.model.Book;
 import org.junit.jupiter.api.*;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -13,6 +16,7 @@ class BookstoreArrayListTest {
     private Book book2;
     private Book book3;
     private Book book4;
+    private Book duplicate;
 
 
     @BeforeEach
@@ -22,6 +26,7 @@ class BookstoreArrayListTest {
         book2 = new Book("9375827462849", "Fahrenheit 451", "Ray Bradbury", 9.99, 2014);
         book3 = new Book("9576818375934", "Hunger Games", "Jane Smith", 10.99, 2015);
         book4 = new Book("9345834573845", "Amulet", "Samantha Smith", 12.99, 2005);
+        duplicate = new Book("9374859192843", "My book", "John Doe", 29.99, 2012);
     }
 
     @Test
@@ -35,6 +40,63 @@ class BookstoreArrayListTest {
         assertNull(bookstore.getMostRecent());
         assertTrue(bookstore.getAllBooks().isEmpty());
         assertEquals(0, bookstore.snapshotArray().length);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Test with one book")
+    void createOneBook() {
+        assertTrue(bookstore.add(book1));
+        assertEquals(1, bookstore.size());
+        assertEquals(book1, bookstore.getMostRecent());
+        assertEquals(book1, bookstore.getMostExpensive());
+        assertEquals(book1, bookstore.getMostRecent());
+        assertTrue(bookstore.getAllBooks().contains(book1));
+        assertFalse(bookstore.getAllBooks().isEmpty());
+        assertEquals(book1, bookstore.findByIsbn(book1.getIsbn()));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test with Duplicate books")
+    void createDuplicateBook() {
+        assertTrue(bookstore.add(book1));
+        assertFalse(bookstore.add(duplicate));
+        assertEquals(1, bookstore.size());
+        assertEquals(book1, bookstore.findByIsbn(book1.getIsbn()));
+        assertEquals("My book", bookstore.findByIsbn(book1.getIsbn()).getTitle());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Search with no title in bookstore")
+    void searchNoResults() {
+        bookstore.add(book1);
+        List<Book> output = bookstore.findByTitle(book2.getTitle());
+        assertEquals(0, output.size());
+        assertNotNull(output); // Supposed to return an empty object instead of null
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Search with no valid author in bookstore")
+    void searchInvalidAuthor() {
+        bookstore.add(book2);
+        List<Book> output = bookstore.findByAuthor(book3.getAuthor());
+        assertEquals(0, output.size());
+        assertNotNull(output);
+        assertEquals("Ray Bradbury", bookstore.findByIsbn(book2.getIsbn()).getAuthor());
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Search with invalid year")
+    void searchInvalidYear() {
+        bookstore.add(book4);
+        List<Book> output = bookstore.findByYear(book2.getYear());
+        assertEquals(0, output.size());
+        assertNotNull(output);
+        assertEquals(2005, bookstore.findByIsbn(book4.getIsbn()).getYear());
     }
 }
 
