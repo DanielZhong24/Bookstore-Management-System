4db5e5021dbc1d706dec295e0c71a689015bcf49
DanielZhong24 <danielzhong0618@gmail.com>
2025-09-29 20:30:15 -0400

    Book testing

--- src/main/java/com/university/bookstore/api/BookstoreAPI.java
+++ src/main/java/com/university/bookstore/api/BookstoreAPI.java
@@ -10,7 +10,7 @@ public interface BookstoreAPI {
     /**
     *a book is added to the bookstore inventory.
     *
-    *@param book the {@link Book) to add
+    *@param book the Book to add
     *@return {@code true} if the book was successfully add, {@code false} otherwise
     */
     boolean add(Book book);
@@ -24,32 +24,32 @@ public interface BookstoreAPI {
     /**
     *will locate the book by its ISBN
     *@param isbn the ISBN to search for
-    *@return the{@link Book} objects, or an empty list if none are found
+    *@return the Book objects, or an empty list if none are found
     */
     Book findByIsbn(String isbn);
     /**
     *finds all books that have titles matching the query given
     *@param titleQuery the tile or partial title to search for
-    *@return a list of matching {@link Book} objects, or an empty list if none are found
+    *@return a list of matching Book objects, or an empty list if none are found
     */
     List<Book> findByTitle(String titleQuery);
     /**
     *locates a book by the author's name
-    *@param athorQuery the author's name or part of their name
-    *@return a link of matching {@link Book} objects, or an empty list if none are found
+    *@param authorQuery the author's name or part of their name
+    *@return a link of matching Book objects, or an empty list if none are found
     */
     List<Book> findByAuthor(String authorQuery);
     /**
     *locates all books in a certain price range
     *@param minPrice the lowest price
     *@param maxPrice the highest price
-    *@return a list of {@link Book} objects within the specified range, or empty list if none are found
+    *@return a list of Book objects within the specified range, or empty list if none are found
     */
     List<Book> findByPriceRange(double minPrice, double maxPrice);
         /**
     *locates all books that were published during a specified year
     *@param year the publication year
-    @return a list of { @link Book} objects that were published during that year or an empty list if there are none
+    @return a list of Book objects that were published during that year or an empty list if there are none
     */
     List<Book> findByYear(int year);
     /** 
@@ -59,17 +59,17 @@ public interface BookstoreAPI {
     int size();
     /**
     *Will get the most expensive book currently in the inventory
-    *@return the {@link Book} with the highest price, or {@code null} if the inventory is empty
+    *@return the Book with the highest price, or {@code null} if the inventory is empty
     **/
     double inventoryValue();
     /**
     * Gets the most expensive book in the inventory
-    *@return the {blank Book} with the most recent publication year, or {@code null} if the inventory is empty
+    *@return the Book with the most recent publication year, or {@code null} if the inventory is empty
     */
     Book getMostExpensive();
     /**
     *gets the most recently published bood in the inventory
-    *@return the {link Book} with the most recent publication year, or {@code null} if the inventory is empty
+    *@return the Book with the most recent publication year, or {@code null} if the inventory is empty
     */
     Book getMostRecent();
 
@@ -81,7 +81,7 @@ public interface BookstoreAPI {
     Book[] snapshotArray();
     /**
     *Gets all the books that are currently in the inventory as a list
-    *@return a list containing all {@link Book} objects
+    *@return a list containing all Book objects
     */
     List<Book> getAllBooks();
 
